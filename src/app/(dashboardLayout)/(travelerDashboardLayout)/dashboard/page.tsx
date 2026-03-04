7310b817586164785848f07f89b6fe29cb679080
Jabed Islam <125015472+developer-jabed@users.noreply.github.com>
2026-03-04 12:39:12 +0600

    test meta

--- src/app/(dashboardLayout)/(travelerDashboardLayout)/dashboard/page.tsx
+++ src/app/(dashboardLayout)/(travelerDashboardLayout)/dashboard/page.tsx
@@ -1,8 +1,6 @@
 "use server";
 import TravelDashboard from '@/components/modules/Meta/Meta'
 
-export const dynamic = "force-dynamic";
-
 export default function TravelerPage() {
   return (
     <div>
